938388311504aed688b79277a83fe207d9454cf3
agaliev <hijhog47@gmail.com>
2020-01-23 18:20:13 +0600

    added project for common objects

---
+++ ShopStore.Common/OperationResult.cs
@@ -0,0 +1,11 @@
+﻿using System;
+
+namespace ShopStore.Common
+{
+    public class OperationResult
+    {
+        public bool Successed { get; set; }
+        public int ErrorCode { get; set; }
+        public string Description { get; set; }
+    }
+}
