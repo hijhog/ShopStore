ba37134df8c8c4f573b20604a772a4de6f5c5e32
agaliev <hijhog47@gmail.com>
2020-01-23 16:07:14 +0600

    added generic repository

---
+++ Data/ShotStore.Data/Interfaces/IRepository.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace ShotStore.Data.Interfaces
+{
+    public interface IRepository<TEntity> where TEntity: class
+    {
+        TEntity Get(int id);
+        IQueryable<TEntity> GetAll();
+        void Insert(TEntity entity);
+        void Update(TEntity entity);
+        void Remove(int id);
+        void Save();
+    }
+}
---
+++ Data/ShotStore.Data/Repositories/Repository.cs
@@ -0,0 +1,54 @@
+﻿using Microsoft.EntityFrameworkCore;
+using ShotStore.Data.Interfaces;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace ShotStore.Data.Repositories
+{
+    public class Repository<TEntity> : IRepository<TEntity> 
+        where TEntity : class
+    {
+        private readonly ApplicationContext _context;
+        private readonly DbSet<TEntity> _table;
+
+        public Repository(ApplicationContext context)
+        {
+            _context = context;
+            _table = context.Set<TEntity>();
+        }
+
+        public TEntity Get(int id)
+        {
+            return _table.Find(id);
+        }
+
+        public IQueryable<TEntity> GetAll()
+        {
+            return _table;
+        }
+
+        public void Insert(TEntity entity)
+        {
+            _table.Add(entity);
+        }
+
+        public void Update(TEntity entity)
+        {
+            _table.Attach(entity);
+            _context.Entry(entity).State = EntityState.Modified;
+        }
+
+        public void Remove(int id)
+        {
+            TEntity entity = _table.Find(id);
+            _table.Remove(entity);
+        }
+
+        public void Save()
+        {
+            _context.SaveChanges();
+        }
+    }
+}
