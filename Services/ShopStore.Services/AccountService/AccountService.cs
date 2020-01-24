59145288bf80d6cae37b50af1527de1d87ed12e9
agaliev <hijhog47@gmail.com>
2020-01-24 12:15:12 +0600

    added account service, is not finished

---
+++ Services/ShopStore.Services/AccountService/AccountService.cs
@@ -0,0 +1,55 @@
+﻿using Microsoft.AspNetCore.Identity;
+using ShopStore.Common;
+using ShopStore.Data.Identity;
+using ShopStore.Services.AccountService.Interfaces;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ShopStore.Services.AccountService
+{
+    public class AccountService : IAccountService
+    {
+        private readonly UserManager<AppUser> _userManager;
+        private readonly SignInManager<AppUser> _signInManager;
+        public AccountService(
+            UserManager<AppUser> userManager,
+            SignInManager<AppUser> signInManager)
+        {
+            _userManager = userManager;
+            _signInManager = signInManager;
+        }
+
+        public async Task<OperationResult> Login(string userName, string password)
+        {
+            var result = new OperationResult() { Description = "Failed to Log In" };
+            try
+            {
+                AppUser user = await _userManager.FindByNameAsync(userName);
+                if(user == null)
+                {
+                    result.Description = "This user does not exist";
+                }
+                else
+                {
+                    SignInResult signInResult = await _signInManager.PasswordSignInAsync(user, password, false, false);
+                    if (signInResult.Succeeded)
+                    {
+                        result.Successed = true;
+                    }
+                }
+            }
+            catch(Exception ex)
+            {
+                result.Description = ex.Message;
+            }
+            return result;
+        }
+
+        public async Task Logout()
+        {
+            await _signInManager.SignOutAsync();
+        }
+    }
+}
---
+++ Web/ShopStore.Web/Controllers/AccountController.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Authentication;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+using ShopStore.Services.AccountService.Interfaces;
+
+namespace ShopStore.Web.Controllers
+{
+    public class AccountController : Controller
+    {
+        private readonly IAccountService _accountService;
+
+        public AccountController(IAccountService accountService)
+        {
+            _accountService = accountService;
+        }
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public async Task<IActionResult> Login(string returnUrl)
+        {
+            await HttpContext.SignOutAsync(IdentityConstants.ExternalScheme);
+            ViewData["ReturnUrl"] = returnUrl;
+            return View();
+        }
+    }
+}
